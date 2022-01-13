2b50475b85a3f630e92be0e23dc77a5dc34e6e3f
Tomasz <krokodyliad@gmail.com>
2022-01-13 21:32:02 +0100

    Improve run.sh script

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-cd build
-cmake ../ && make && cd .. && ./bin/game
+cd `dirname $0`
+mkdir -p build
+cd build && cmake ../ && make
+SUCCESS=`echo -n $?`
+if [[ $SUCCESS == "0" ]]; then
+    cd ..
+    if [[ $1 == "debug" ]]; then
+        gdb bin/game
+    else
+        bin/game
+    fi
+fi
